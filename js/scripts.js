071e726a73b36bedfaa585fb7be0742761809630
Kate Goss and Liza Tennis <liza@>
2018-08-20 13:30:24 -0700

    add extra address code

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,36 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
 }
-
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
 
 
 // user interface logic
 $(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+      '<div class="form-group">' +
+          '<label for="new-street">Street</label>' +
+          '<input type="text" class="form-control" id="new-street">' +
+        '</div>' +
+        '<div class="form-group">' +
+          '<label for="new-city">City</label>' +
+          '<input type="text" class="form-control" id="new-city">' +
+        '</div>' +
+        '<div class="form-group">' +
+          '<label for="new-state">State</label>' +
+          '<input type="text" class="form-control" id="new-state">' +
+        '</div>')
+    });
+
+
+
+
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
