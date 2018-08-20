cf413baed65f6ab881e7a88791b048e354784834
Kate Goss and Liza Tennis <liza@>
2018-08-20 13:50:11 -0700

    add multiple addresses and output results

--- js/scripts.js
+++ js/scripts.js
@@ -40,6 +40,16 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input#new-street").val();
+      var inputtedCity = $(this).find("input#new-city").val();
+      var inputtedState = $(this).find("input#new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      newContact.addresses.push(newAddress);
+    });
+
+
+
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
 
     // $("input#new-first-name").val("");
@@ -50,6 +60,17 @@ $(document).ready(function() {
     $("#show-contact h2").text(newContact.firstName);
     $(".first-name").text(newContact.firstName);
     $(".last-name").text(newContact.lastName);
+    $("ul#addresses").text("");
+    newContact.addresses.forEach(function(address) {
+      $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.state + "</li>");
+    })
+
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
     });
   });
 });
